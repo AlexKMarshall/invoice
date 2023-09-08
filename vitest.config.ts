10f0015b1117efa75e67f0467eb5cebf5ca34467
AlexKMarshall <alexmarshall@qogita.com>
2023-09-08 15:45:27 +0100

    fix: typo

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
-    include: ["**/src/**/*"],
+    include: ["**/app/**/*"],
   },
 });
