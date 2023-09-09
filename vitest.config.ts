896ef22b0cc31dcb2c7a95b1d0cf6d52ff5745a7
AlexKMarshall <alexmarshall@qogita.com>
2023-09-09 07:51:26 +0100

    test: update vite config includes

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
-    include: ["**/app/**/*"],
+    include: ["**/*.test.(ts|tsx)"],
   },
 });
