0021b006b94999afc800b4c698e6be576be9a56e
AlexKMarshall <alexmarshall@qogita.com>
2023-09-13 08:42:47 +0100

    feat: add total count summary

--- app/routes/invoices.tsx
+++ app/routes/invoices.tsx
@@ -7,14 +7,23 @@ import { getInvoiceListItems } from "~/models/invoice.server";
 export async function loader() {
   const invoiceListItems = await getInvoiceListItems();
 
-  return json({ invoiceListItems });
+  return json({ invoiceListItems, count: invoiceListItems.length });
+}
+
+function pluralize(word: string, count: number, plural?: string) {
+  plural ??= `${word}s`;
+  return count === 1 ? word : plural;
 }
 
 export default function Invoices() {
-  const { invoiceListItems } = useLoaderData<typeof loader>();
+  const { invoiceListItems, count } = useLoaderData<typeof loader>();
   return (
     <main>
       <h1>Invoices</h1>
+      <p>
+        There {pluralize("is", count, "are")} {count} total{" "}
+        {pluralize("invoice", count)}
+      </p>
       <Link to="new">New invoice</Link>
       <Outlet />
       {invoiceListItems.length ? (
@@ -22,7 +31,6 @@ export default function Invoices() {
           {invoiceListItems.map((invoice) => (
             <li key={invoice.id}>
               <div>#{invoice.fid}</div>
-              <div>{invoice.id}</div>
               <div>{invoice.clientName}</div>
               <div>{invoice.total}</div>
               <div>{format(new Date(invoice.dueDate), "y-MM-dd")}</div>
