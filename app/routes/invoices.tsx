345566d24d275f6a7a3012e30a86c08a151ba8ee
AlexKMarshall <alexmarshall@qogita.com>
2023-09-17 08:22:40 +0100

    style: basic list layout

--- app/routes/invoices.tsx
+++ app/routes/invoices.tsx
@@ -1,39 +1,62 @@
 import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
+import { Button } from "~/components/ui/button";
 import { getInvoiceListItems } from "~/models/invoice.server";
+
+function pluralize(word: string, pluralVersion = `${word}s`) {
+  return (count: number, includeCount = false) => {
+    const prefix = includeCount ? `${count} ` : "";
+    return count === 1 ? `${prefix}${word}` : `${prefix}${pluralVersion}`;
+  };
+}
+
+const pluralIs = pluralize("is", "are");
+const pluralInvoice = pluralize("invoice");
 
 export async function loader() {
   const invoiceListItems = await getInvoiceListItems();
+  const count = invoiceListItems.length;
 
-  return json({ invoiceListItems, count: invoiceListItems.length });
-}
-
-function pluralize(word: string, count: number, plural?: string) {
-  plural ??= `${word}s`;
-  return count === 1 ? word : plural;
+  return json({
+    invoiceListItems,
+    count,
+    subheading: {
+      base: pluralInvoice(count, true),
+      sm: `There ${pluralIs(count)} ${count} total ${pluralInvoice(count)}`,
+    },
+  });
 }
 
 export default function Invoices() {
-  const { invoiceListItems, count } = useLoaderData<typeof loader>();
+  const { invoiceListItems, subheading } = useLoaderData<typeof loader>();
   return (
-    <main>
-      <h1>Invoices</h1>
-      <p>
-        There {pluralize("is", count, "are")} {count} total{" "}
-        {pluralize("invoice", count)}
-      </p>
-      <Link to="new">New invoice</Link>
+    <main className="px-6 py-8">
+      <div className="flex mb-8 items-center">
+        <div className="flex-grow">
+          <h1 className="font-bold text-3xl">Invoices</h1>
+          <p className="sm:hidden">{subheading.base}</p>
+          <p className="hidden sm:block">{subheading.sm}</p>
+        </div>
+        <Button asChild>
+          <Link to="new">New invoice</Link>
+        </Button>
+      </div>
       <Outlet />
       {invoiceListItems.length ? (
-        <ul>
+        <ul className="flex flex-col gap-4">
           {invoiceListItems.map((invoice) => (
-            <li key={invoice.id}>
-              <div>#{invoice.fid}</div>
-              <div>{invoice.clientName}</div>
-              <div>{invoice.total}</div>
-              <div>{invoice.dueDate}</div>
-              <div>{invoice.status}</div>
+            <li
+              key={invoice.id}
+              className="grid grid-cols-2 rounded-lg bg-gray-200 p-6 gap-6"
+            >
+              <h2 className="before:content-['#']">{invoice.fid}</h2>
+              <p className="justify-self-end">{invoice.clientName}</p>
+              <div className="self-end">
+                <p>{invoice.total}</p>
+                <p>{invoice.dueDate}</p>
+              </div>
+              <p className="justify-self-end self-end">{invoice.status}</p>
             </li>
           ))}
         </ul>
