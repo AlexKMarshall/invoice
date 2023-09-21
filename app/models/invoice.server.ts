68d060d017a86e1e5494e87e9712e16d4a702ff6
AlexKMarshall <alexmarshall@qogita.com>
2023-09-21 18:12:23 +0100

    feat: show basic invoice detail

--- app/models/invoice.server.ts
+++ app/models/invoice.server.ts
@@ -78,12 +78,56 @@ export async function getInvoiceDetail({
 }) {
   const rawInvoice = await prisma.invoice.findUnique({
     where,
+    select: {
+      id: true,
+      fid: true,
+      projectDescription: true,
+      billFromStreet: true,
+      billFromCity: true,
+      billFromPostCode: true,
+      billFromCountry: true,
+      invoiceDate: true,
+      clientName: true,
+      clientEmail: true,
+      billToStreet: true,
+      billToCity: true,
+      billToPostCode: true,
+      billToCountry: true,
+      paymentTerm: {
+        select: {
+          days: true,
+        },
+      },
+    },
   })
 
   return InvoiceModel.pick({
     id: true,
     fid: true,
-  }).parse(rawInvoice)
+    projectDescription: true,
+    billFromStreet: true,
+    billFromCity: true,
+    billFromPostCode: true,
+    billFromCountry: true,
+    invoiceDate: true,
+    clientName: true,
+    clientEmail: true,
+    billToStreet: true,
+    billToCity: true,
+    billToPostCode: true,
+    billToCountry: true,
+  })
+    .extend({
+      paymentTerm: PaymentTermModel.pick({ days: true }),
+    })
+    .transform(({ paymentTerm, ...invoice }) => ({
+      ...invoice,
+      dueDate: format(
+        add(new Date(invoice.invoiceDate), { days: paymentTerm.days }),
+        'dd MMM yyyy',
+      ),
+    }))
+    .parse(rawInvoice)
 }
 
 export function getPaymentTerms() {
--- app/routes/invoices_.$fid.tsx
+++ app/routes/invoices_.$fid.tsx
@@ -3,6 +3,7 @@ import { useLoaderData } from '@remix-run/react'
 
 import { Heading } from '~/components/ui/heading'
 import { InvoiceFid } from '~/components/ui/invoiceFid'
+import { Text } from '~/components/ui/text'
 import { getInvoiceDetail } from '~/models/invoice.server'
 import { InvoiceModel } from '~/schemas'
 
@@ -30,9 +31,30 @@ export default function InvoiceDetail() {
   const { invoice } = useLoaderData<typeof loader>()
   return (
     <main className="mx-auto flex min-h-screen max-w-4xl flex-col px-6 py-8">
-      <Heading level={2} className="font-bold">
+      <Heading level={1} className="font-bold">
         <InvoiceFid fid={invoice.fid} />
       </Heading>
+      <Text>{invoice.projectDescription}</Text>
+      <address>
+        <span>{invoice.billFromStreet}</span>
+        <span>{invoice.billFromCity}</span>
+        <span>{invoice.billFromPostCode}</span>
+        <span>{invoice.billFromCountry}</span>
+      </address>
+      <Heading level={2}>Invoice Date</Heading>
+      <Text>{invoice.invoiceDate}</Text>
+      <Heading level={2}>Payment Due</Heading>
+      <Text>{invoice.dueDate}</Text>
+      <Heading level={2}>Bill To</Heading>
+      <Text>{invoice.clientName}</Text>
+      <address>
+        <span>{invoice.billToStreet}</span>
+        <span>{invoice.billToCity}</span>
+        <span>{invoice.billToPostCode}</span>
+        <span>{invoice.billToCountry}</span>
+      </address>
+      <Heading level={2}>Sent To</Heading>
+      <Text>{invoice.clientEmail}</Text>
     </main>
   )
 }
